e3188d4a597a6a1896a6f044ba8b5a6481da236f
alexander-daniel <alex@lesinstruments.com>
2014-04-05 12:03:24 -0400

    additions

--- arduino/README.md
+++ arduino/README.md
@@ -1,5 +1,5 @@
 #Notes on Usage
-Got a new Arduino? Have a sensor? Great! You'll be visualizing your sensor data in no time.
+Got a new Arduino? Have a sensor? Great! You'll be visualizing your sensor data in no time! No wifi shield, ethernet shield or voodoo magic required.
 It's easy!
 
 Thanks to Node.js and the Johnny-Five library, you can easily interface with your Arduino and get started working on things right away!
@@ -10,12 +10,19 @@ Thanks to Node.js and the Johnny-Five library, you can easily interface with you
 ####Install node.js
 [Download Here](http://nodejs.org/download)
 
-####Create a folder for your sensor project and move into it
+####Download and install the latest Arduino software
+[Download Here](http://arduino.cc/en/main/software)
+
+####Launch the Arduino IDE, and upload the 'Standard Firmata' sketch to your board.
+Once uploaded, close the Arduino IDE. We're done with it. The 'Standard Firmata' sketch is all we need to upload to allow any computer with node.js to communicate with it!
+
+####In your terminal, create a folder for your sensor project and move into it
 ```
 $ mkdir your-project-name
 $ cd your-project-name
 ```
 ####Use the node package manager (npm) to install the required libraries.
+node.js is awesome and will only install these modules in your project directory. It's easy to keep things modular and organized!
 ```
 $ npm install plotly
 $ npm install johnny-five
@@ -24,4 +31,4 @@ $ npm install johnny-five
 ```
 $ node example-script.js
 ```
-Johnny-Five will automatically connect to your Arduino, and start streaming all the data to Plotly!
+Johnny-Five will automatically connect to your Arduino over serial, and start streaming all the data to Plotly!
---
+++ arduino/waterflow/waterflow.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include "plotly_streaming_ethernet.h"
+#define flow_sensor_pin 2
+// count how many pulses!
+volatile uint16_t pulses = 0;
+// track the state of the pulse pin
+volatile uint8_t lastflowpinstate;
+// you can try to keep time of how long it is between pulses
+volatile uint32_t lastflowratetimer = 0;
+// and use that to calculate a flow rate
+volatile float flowrate;
+// Interrupt is called once a millisecond, looks for any pulses from the sensor!
+SIGNAL(TIMER0_COMPA_vect) {
+  uint8_t x = digitalRead(FLOWSENSORPIN);
+
+  if (x == lastflowpinstate) {
+    lastflowratetimer++;
+    return; // nothing changed!
+  }
+
+  if (x == HIGH) {
+    //low to high transition!
+    pulses++;
+  }
+  lastflowpinstate = x;
+  flowrate = 1000.0;
+  flowrate /= lastflowratetimer;  // in hertz
+  lastflowratetimer = 0;
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+  }
+}
+
+// Sign up for plotly here: https://plot.ly
+// View your API key and streamtokens here: https://plot.ly/settings
+#define nTraces 1
+// View your tokens here: https://plot.ly/settings
+// Supply as many tokens as data traces
+// e.g. if you want to ploty A0 and A1 vs time, supply two tokens
+char *tokens[nTraces] = {"stream_token"};
+// arguments: username, api key, streaming token, filename, # of traces
+plotly graph("username", "api_key", tokens, "filename", nTraces);
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte my_ip[] = { 199, 168, 222, 18 }; // google will tell you: "public ip address"
+void startEthernet(){
+    Serial.println("... Initializing ethernet");
+    if(Ethernet.begin(mac) == 0){
+        Serial.println("... Failed to configure Ethernet using DHCP");
+        // no point in carrying on, so do nothing forevermore:
+        // try to congifure using IP address instead of DHCP:
+        Ethernet.begin(mac, my_ip);
+    }
+    Serial.println("... Done initializing ethernet");
+    delay(1000);
+}
+
+
+void setup() {
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  pinMode(flow_sensor_pin, INPUT);
+  digitalWrite(flow_sensor_pin, HIGH);
+  lastflowpinstate = digitalRead(flow_sensor_pin);
+  useInterrupt(true);
+  startEthernet();
+  graph.init();
+  graph.openStream();
+}
+
+unsigned long x;
+int y;
+
+void loop() {
+
+  Serial.print("Freq: "); Serial.println(flowrate);
+  Serial.print("Pulses: "); Serial.println(pulses, DEC);
+
+  // if a plastic sensor use the following calculation
+  // Sensor Frequency (Hz) = 7.5 * Q (Liters/min)
+  // Liters = Q * time elapsed (seconds) / 60 (seconds/minute)
+  // Liters = (Frequency (Pulses/second) / 7.5) * time elapsed (seconds) / 60
+  // Liters = Pulses / (7.5 * 60)
+  float liters = pulses;
+  liters /= 7.5;
+  liters /= 60.0;
+
+/*
+  // if a brass sensor use the following calculation
+  float liters = pulses;
+  liters /= 8.1;
+  liters -= 6;
+  liters /= 60.0;
+*/
+  Serial.print(liters); Serial.println(" Liters");
+  lcd.setCursor(0, 1);
+  lcd.print(liters); lcd.print(" Liters        ");
+
+
+  graph.plot(millis(), liters, tokens[0]);
+  delay(100);
+}
