6882ec11a95ab84c1e8c7dc6f91c3cc22769c3f9
Chris P <christopher.parmer@gmail.com>
2014-05-09 19:13:36 -0400

    Fix indentation

--- raspberrypi/tmp36/tmp36.py
+++ raspberrypi/tmp36/tmp36.py
@@ -27,27 +27,27 @@ readadc.initialize()
 stream = py.Stream(plotly_user_config['plotly_streaming_tokens'][0])
 stream.open()
 
-#the main sensor reading loop
+#the main sensor reading and plotting loop
 while True:
-        sensor_data = readadc.readadc(sensor_pin,
-                                      readadc.PINS.SPICLK,
-                                      readadc.PINS.SPIMOSI,
-                                      readadc.PINS.SPIMISO,
-                                      readadc.PINS.SPICS)
+    sensor_data = readadc.readadc(sensor_pin,
+                                  readadc.PINS.SPICLK,
+                                  readadc.PINS.SPIMOSI,
+                                  readadc.PINS.SPIMISO,
+                                  readadc.PINS.SPICS)
 
-        millivolts = sensor_data * (3300.0 / 1024.0)
-        # 10 mv per degree
-        temp_C = ((millivolts - 100.0) / 10.0) - 40.0
-        # convert celsius to fahrenheit
-        temp_F = (temp_C * 9.0 / 5.0) + 32
-        # remove decimal point from millivolts
-        millivolts = "%d" % millivolts
-        # show only one decimal place for temprature and voltage readings
-        temp_C = "%.1f" % temp_C
-        temp_F = "%.1f" % temp_F
+    millivolts = sensor_data * (3300.0 / 1024.0)
+    # 10 mv per degree
+    temp_C = ((millivolts - 100.0) / 10.0) - 40.0
+    # convert celsius to fahrenheit
+    temp_F = (temp_C * 9.0 / 5.0) + 32
+    # remove decimal point from millivolts
+    millivolts = "%d" % millivolts
+    # show only one decimal place for temprature and voltage readings
+    temp_C = "%.1f" % temp_C
+    temp_F = "%.1f" % temp_F
 
-        # write the data to plotly
-        stream.write({'x': datetime.datetime.now(), 'y': temp_C})
+    # write the data to plotly
+    stream.write({'x': datetime.datetime.now(), 'y': temp_C})
 
-        # delay between stream posts
-        time.sleep(0.25)
+    # delay between stream posts
+    time.sleep(0.25)
